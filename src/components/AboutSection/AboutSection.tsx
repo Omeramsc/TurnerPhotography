8c5cd766d2bc43e06073a2aa0ae3bc5cca1c95c8
Idan Levian <eidan47@gmail.com>
2024-11-03 17:23:11 +0200

    Adding credit to photos text

--- src/components/AboutSection/AboutSection.tsx
+++ src/components/AboutSection/AboutSection.tsx
@@ -12,13 +12,14 @@ export const AboutSection: React.FC<AboutSectionProps> = ({active, removeIsActiv
         <div className="about-close hover-target" onClick={removeIsActive}/>
         <div className="section-center">
             <div className="container">
-                <div className="row justify-content-center">
-                    <div className="col-12 text-center">
+                <div className="row justify-content-center flex-column align-items-center">
+                    <div className="col-12 text-center d-flex flex-column image-section p-2">
                         <img src={require("../../assets/main/aboutme.jpg")} className="w-100 h-auto img-thumbnail"
                              style={{maxWidth: '350px'}} alt="img-thumbnail"/>
+                        <span className='credit-text'>Photo by ___</span>
                     </div>
                     <div className="col-lg-8 text-center mt-4">
-                        <p>
+                    <p>
                             A passionate photographer dedicated to capturing the energy and
                             essence of live music. Showcased here are moments that bring
                             forward the concert experience to life - allowing others to
--- src/components/ContactSection/ContactSection.tsx
+++ src/components/ContactSection/ContactSection.tsx
@@ -13,25 +13,26 @@ export const ContactSection: React.FC<ContactSectionProps> = ({active, removeIsA
     <div className={`contact-section ${active ? "active" : ""}`}>
         <div className="contact-close hover-target" onClick={removeIsActive}/>
         <div className="section-center">
-            <div className="container">
-                <div className="row justify-content-center">
-                    <div className="col-12 text-center">
+            <div className="container d-flex flex-column justify-content-center align-content-center">
+                <div className="row justify-content-center align-content-center">
+                    <div className="col-12 text-center d-flex flex-column image-section p-2">
                         <img
                             src={require('../../assets/main/frame-sized.jpg')}
                             className="w-100 h-auto"
                             style={{maxWidth: '750px'}}
                             alt="Contact"
                         />
+                    <span className='credit-text'>Photo by ___</span>
                     </div>
                     <a href="mailto:omeramsc@gmail.com" className="hover-target text-center">
                         omeramsc@gmail.com
                     </a>
                 </div>
                 <div className="col-12 text-center mt-4 social">
-                    <a href="https://www.instagram.com/omerams.live/" className="hover-target">
+                    <a href="https://www.instagram.com/omerams.live/" className="hover-target" target="_blank" rel="noopener noreferrer">
                         <FontAwesomeIcon icon={faInstagram} className="mr-2"/> Instagram
                     </a>
-                    <a href="https://www.facebook.com/ChibaMeta/" className="hover-target">
+                    <a href="https://www.facebook.com/ChibaMeta/" className="hover-target" target="_blank" rel="noopener noreferrer">
                         <FontAwesomeIcon icon={faFacebook} className="mr-2"/> Facebook
                     </a>
                 </div>
