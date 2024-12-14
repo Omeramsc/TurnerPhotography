c29126459a0142b30aa79c4180f97f78c99d2969
Omer <omeramsc@gmail.com>
2024-12-14 16:05:31 +0200

    chore: add 404 and provide small fix
    Signed-off-by: Omer <omeramsc@gmail.com>

---
+++ src/components/NotFound/NotFound.tsx
@@ -0,0 +1,28 @@
+// src/components/NotFoundPage.js
+import { faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './NotFound.css'; // You can style the page using a separate CSS file
+
+const NotFoundPage = () =>
+    (
+        <div className="not-found-container">
+            <div className="not-found-content">
+                <h1>Will be back shortly!</h1>
+                <p>This page is currently under construction</p>
+                <div className="watermark">
+                    <img src="../../src/assets/main/watermark_white.png" alt="Watermark" />
+                </div>
+                <div className="col-12 text-center mt-4 social">
+                    <a href={`https://www.instagram.com/chibameta/`} className="" target="_blank" rel="noopener noreferrer">
+                        <FontAwesomeIcon icon={faInstagram} className="mr-2"/> Instagram
+                    </a>
+                    <a href="https://www.facebook.com/ChibaMeta/" className="" target="_blank" rel="noopener noreferrer">
+                        <FontAwesomeIcon icon={faFacebook} className="mr-2"/> Facebook
+                    </a>
+                </div>
+                <p><a href="mailto:omeramsc@gmail.com">omeramsc@gmail.com</a></p>
+            </div>
+        </div>
+    );
+
+export default NotFoundPage;
